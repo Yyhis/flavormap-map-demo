702a486398a7fb3bd374297a76e879423586b043
hyss <oouoo717@gmail.com>
2024-11-02 20:55:02 +0900

    init : project-map demo

---
+++ src/main/java/net/yyhis/flavormap_map_demo/FlavormapMapDemoApplication.java
@@ -0,0 +1,13 @@
+package net.yyhis.flavormap_map_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavormapMapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavormapMapDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yyhis/flavormap_map_demo/FlavormapMapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yyhis.flavormap_map_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavormapMapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
