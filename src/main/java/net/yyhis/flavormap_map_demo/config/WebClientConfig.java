19bb9293ceff492422ee7e8c58938c41eb7850bc
hyss <oouoo717@gmail.com>
2024-11-06 22:29:15 +0900

    feat: Add config for WebClient

---
+++ src/main/java/net/yyhis/flavormap_map_demo/config/WebClientConfig.java
@@ -0,0 +1,13 @@
+package net.yyhis.flavormap_map_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
